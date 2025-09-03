007d798ef90ac4a7e8763d7dbc8a5e563fdb0bb3
Prachi Sharnagat <prachisharnagat06@gmail.com>
2025-09-03 13:09:33 +0530

    Basic one page portfolio with contact links

---
+++ 14-BasicPortfolio/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+     fontFamily: {
+        roboto: ['"Roboto"', 'sans-serif'],
+      },
+       fontFamily: {
+        poppins: ['"Poppins"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
